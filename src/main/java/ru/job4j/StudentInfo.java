0fe271d9445fea056568ac2b00762c0c1ec78fea
oyvey1984 <ayrat.tazetdinov@mail.ru>
2022-09-06 23:02:19 +0500

    3.2. Загрузка файлов на github [#243562 #339987]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ayrat Tazetdinov");
+        System.out.println("24.04.1989");
+
+    }
+}
