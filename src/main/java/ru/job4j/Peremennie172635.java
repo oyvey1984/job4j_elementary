f4f4ab7181310af5e547fd116b7d49d11bd3efb0
oyvey1984 <ayrat.tazetdinov@mail.ru>
2022-09-07 18:42:37 +0500

    2. Переменные. [#172635 #340742]

---
+++ src/main/java/ru/job4j/Peremennie172635.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Peremennie172635 {
+    public static void main(String[] args) {
+        float age = 0.5F;
+        char gender = 'Ж';
+        float growth = 1.76F;
+        short bricks = 28458;
+        int bacteria = 1000000000;
+        long andromeda = 5000000054545000L;
+        boolean work = true;
+        byte file = 4;
+    }
+}
