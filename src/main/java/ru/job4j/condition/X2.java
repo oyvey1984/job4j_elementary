94bfb23025f58a6bc69a2a04826965533081d0e2
oyvey1984 <ayrat.tazetdinov@mail.ru>
2022-09-29 00:04:23 +0500

    1.2. Входные данные в тесте [#297750 #346939]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,7 +5,6 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
-
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -49,7 +49,6 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
 
-
     @Test
     public void whenA1B1C1X0Then1() {
         int a = 1;
