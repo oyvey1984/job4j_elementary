7c009269a036e7afd84cfbe99dccb81a83201794
oyvey1984 <ayrat.tazetdinov@mail.ru>
2022-12-26 20:30:25 +0500

    3. Перегрузить метод max для трех чисел [#122644 #374844]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,12 @@ public class Max {
     public static int max(int left, int right) {
         return left >= right ? left : right;
     }
+
+    public static int max(int left, int right, int midl) {
+        return max(left, right) >= midl ? max(left, right) : midl;
+    }
+
+    public static int max(int left, int right, int midl, int down) {
+        return max(left, right, midl) >= down ? max(left, right, midl) : down;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 public class MaxTest {
     @Test
@@ -28,6 +29,27 @@ public class MaxTest {
         int right = 7;
         int result = Max.max(left, right);
         int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax100To25And50And100() {
+        int left = 25;
+        int right = 50;
+        int midl = 100;
+        int result = Max.max(left, right, midl);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax999To3And33And333And999() {
+        int left = 3;
+        int right = 33;
+        int midl = 333;
+        int down = 999;
+        int result = Max.max(left, right, midl, down);
+        int expected = 999;
         assertThat(result).isEqualTo(expected);
     }
 }
